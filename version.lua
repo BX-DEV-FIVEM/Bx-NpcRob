369e3ba897ca346e112f2dcb97a11e2145c46a94
BX-DEV <140925178+BX-DEV-FIVEM@users.noreply.github.com>
2024-09-22 12:52:44 +0200

    Update version.lua

--- version.lua
+++ version.lua
@@ -1,51 +1,47 @@
-
-
-local version = 'V2.0'
+local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version', 0)
+local resourceName = GetCurrentResourceName()
 local expectedResourceName = "BX-NpcRob"
 
-if Config.CheckUpdate then
+local versionURL = "https://raw.githubusercontent.com/BX-DEV-FIVEM/BX.Version/refs/heads/main/version.lua"
 
-    CreateThread(function()
-        Wait(5000)
-        local resourceName = GetCurrentResourceName()
-        local currentVersion = GetResourceMetadata(resourceName, 'version', 0)
-        local versionUrl = 'https://raw.githubusercontent.com/BX-DEV-FIVEM/BX-NpcRob/main/version.lua'
+ if GetCurrentResourceName() == expectedResourceName then
+PerformHttpRequest(versionURL, function(err, response, headers)
+    if err == 200 then
+        local func, loadErr = load(response)
+        if func then
+            func() 
+            if version then
+                local remoteVersion = version["BX-NpcRob"]
 
-        PerformHttpRequest(versionUrl, function(error, result, headers)
-            if error ~= 200 then
-                print("^1Error checking version: GitHub is having issues or the manifest file is not accessible.^0")
-                return
-            end
+                if remoteVersion ~= currentVersion then
+                        print("  //\n  || ^1   " .. resourceName .. "^0 from ^5BX-DEV^0")
+                        print("  ||    Last Version: ❌ ")
+                        print(string.format("  ||    ^3New version available!^0 Current Version: ^1%s^0, Latest Version: ^2%s^0  ", currentVersion, remoteVersion))
+                        print("  || ^5   https://keymaster.fivem.net ^0\n  \\\\")
+   
+                else
+                    print("^2[INFO]^1 " .. resourceName .. " ^0from ^5BX-DEV^0 is update ✔️")
+                            
 
-            -- Pattern pour extraire la version depuis le contenu de version.lua
-            local latestVersion = result:match("local version = ['\"](%S+)['\"]")
-
-            if latestVersion == nil then
-                print("^1Error: Version information could not be found in the version file.^0")
-                return
-            end
-            
-
-            if currentVersion ~= latestVersion then
-                print("  //\n  || ^1   " .. resourceName .. "^0 from ^5BX-DEV^0")
-                print("  ||    Last Version: ❌ ")
-                print(string.format("  ||    ^3New version available!^0 Current Version: ^5%s^0, Latest Version: ^2%s^0  ", currentVersion, latestVersion))
-                print("  || ^5   https://github.com/BX-DEV-FIVEM/BX-NpcRob ^0\n  \\\\")
+                end
             else
-                print("  //\n  || ^1   " .. resourceName .. "^0 from ^5BX-DEV^0")
-                print("  ||    Version : ^2" .. latestVersion .. "^0")
-                print("  ||    Last Version: ✔️ \n  \\\\")
+                print("^1[ERROR]^7 Unable to retrieve remote version for" .. resourceName .. ".")
             end
-        end, 'GET')
-    end)
-end
+        else
+            print("^1[ERROR]^7 Error running remote script: " .. loadErr)
+        end
+    else
+        print("^1[ERROR]^7Unable to retrieve remote version for " .. resourceName .. ". error: " .. err)
+    end
+end)
+    
+    end
 
-    AddEventHandler('onResourceStart', function(resourceName)
+
+
+AddEventHandler('onResourceStart', function(resourceName)
         if GetCurrentResourceName() ~= expectedResourceName then
-            print("^1  Ressource Name must be 'BX-NpcRob' ^0")
+            print("^1 [ERROR] ^3 Ressource Name must be ".. expectedResourceName .." ^0")
         end
     
     end)
-    
-    
-    
